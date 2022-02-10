387de7416d010c6133c412cf1211b0c2b584a65a
Politicooo48 <99099140+Politicooo48@users.noreply.github.com>
2022-02-10 21:22:15 +0100

    Update

--- index.js
+++ index.js
@@ -154,3 +154,19 @@ client.on("guildMemberRemove", member => {
             })
     }
 })
+const ytch = require('yt-channel-info');
+client.on("messageCreate", message => {
+    if (message.content == "dep!lastvideo") {
+        const channelId = 'idCanaleYoutube' //Settare id del tuo canale YouTube
+        ytch.getChannelVideos(channelId, "newest").then((response) => {
+            var embed = new Discord.MessageEmbed()
+                .setTitle(response.items[0].title)
+                .setURL("https://www.youtube.com/watch?v=" + response.items[0].videoId)
+                .setThumbnail(response.items[0].videoThumbnails[3].url)
+                .addField("Views", response.items[0].viewCount.toString(), true)
+                .addField("Duration", response.items[0].durationText, true)
+                .addField("Published", response.items[0].publishedText, true)
+            message.channel.send({embeds: [embeds]})
+        })
+    }
+})
