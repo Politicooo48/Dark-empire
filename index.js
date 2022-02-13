5bed3bcd30b0943d867c83503b066e7b127e5a7e
Politicooo48 <99099140+Politicooo48@users.noreply.github.com>
2022-02-13 18:34:02 +0100

    Update

--- index.js
+++ index.js
@@ -189,3 +189,16 @@ client.on("messageCreate", message => {
         message.channel.send({ embeds: [embed] })
     }
 })
+client.on("messageCreate", message => {
+    if (message.content == "dep!ticket") {
+        var button1 = new Discord.MessageButton()
+            .setLabel("Apri ticket")
+            .setCustomId("apriTicket")
+            .setStyle("PRIMARY")
+
+        var row = new Discord.MessageActionRow()
+            .addComponents(button1)
+
+        message.channel.send({ content: "Clicca sul bottone per verificarti", components: [row] })
+    }
+})
